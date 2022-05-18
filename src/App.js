c8558fac81114dd3f3e11893457ea9ac35c324c5
lekshmi12345 <lekshmisnair213@gmail>
2022-05-18 10:04:24 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import Productentry from './components/Productentry';
+import Productsearch from './components/Productsearch';
 function App() {
   return (
     <div>
-
+<Productsearch/>
 <Productentry/>
 
 
