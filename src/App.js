4a68ff209de2c38e254dea5cd64f317c03b6233a
lekshmi12345 <lekshmisnair213@gmail>
2022-05-18 12:12:37 +0530

    update commit

--- src/App.js
+++ src/App.js
@@ -5,11 +5,19 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import Productentry from './components/Productentry';
 import Productsearch from './components/Productsearch';
 import Viewproduct from './components/Viewproduct';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 function App() {
   return (
     <div>
 
-<Viewproduct/>
+<BrowserRouter>
+<Routes>
+
+<Route path="/" exact element={<Productentry/>}/>
+<Route path="/search" exact element={<Productsearch/>}/>
+<Route path="/view" exact element={<Viewproduct/>}/>
+     </Routes>
+</BrowserRouter>
 
 
     </div>
